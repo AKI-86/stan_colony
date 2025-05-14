5b6786c0213f77d2ed8bc269c3b93c94026fd649
AKI-86 <201310899+AKI-86@users.noreply.github.com>
2025-05-14 10:34:27 +0000

    Deviseまわり、退会機能、レイアウト編集

--- db/migrate/20250514102841_add_references_to_topics.rb
+++ db/migrate/20250514102841_add_references_to_topics.rb
@@ -1,5 +1,5 @@
 class AddReferencesToTopics < ActiveRecord::Migration[6.1]
   def change
-    add_reference :topic, :topic_genre, null: false, foreign_key: true
+    add_reference :topics, :topic_genre, null: false, foreign_key: true
   end
 end
