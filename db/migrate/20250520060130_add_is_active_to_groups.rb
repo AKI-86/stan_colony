b135e5a8678cffd89e5c06e51932b8e09b7f1232
AKI-86 <201310899+AKI-86@users.noreply.github.com>
2025-05-24 09:22:45 +0000

    レイアウト修正

--- db/migrate/20250520060130_add_is_active_to_groups.rb
+++ db/migrate/20250520060130_add_is_active_to_groups.rb
@@ -1,5 +1,5 @@
 class AddIsActiveToGroups < ActiveRecord::Migration[6.1]
   def change
-    add_column :reports, :status, :integer, default: 0, null: false
+    add_column :groups, :is_active, :boolean, default: true, null: false
   end
 end
